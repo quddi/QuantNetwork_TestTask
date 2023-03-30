afd97121c74105f0df3c30577117cc633467e137
Nikita Andrushko <alemkhf12@gmail.com>
2023-03-30 21:56:57 +0300

    Added ground checker zone visualization

--- Assets/Scripts/Model/Car/GroundChecker.cs
+++ Assets/Scripts/Model/Car/GroundChecker.cs
@@ -14,16 +14,6 @@ public class GroundChecker : MonoBehaviour
         IsGrounded = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        //IsGrounded = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        //IsGrounded = false;
-    }
-
     private void FixedUpdate()
     {
         IsGrounded =Physics2D.BoxCast(_boxCollider2D.bounds.center, 
@@ -32,20 +22,12 @@ public class GroundChecker : MonoBehaviour
             Vector2.down,
             distance: 0.1f,
             _groundLayerMask);
-        
-        Debug.Log(IsGrounded);
     }
     
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        
-        Vector3 size = new Vector3(_boxCollider2D.size.x, _boxCollider2D.size.y, 0f);
-        Vector3 center = new Vector3(_boxCollider2D.offset.x, _boxCollider2D.offset.y, 0f) + transform.position;
-        
-        Gizmos.DrawWireCube(center, size);
-        
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(center, center + (Vector3.down * 0.1f));
+
+        Gizmos.DrawWireCube(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size);
     }
 }
